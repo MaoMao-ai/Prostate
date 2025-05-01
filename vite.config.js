bd0a2d831a5dfdbaad833f53f488e22239d5ae19
Dylan <ym241@duke.edu>
2025-05-01 18:07:47 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Prostate/',  // 一定要加上你的 repo 名称
+  base: '/Prostate/',  // GitHub Pages 需要仓库名称作为基础路径
   plugins: [react()],
 });
